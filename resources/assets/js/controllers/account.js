c4a61ee7c9fd64e1da88306ca9604dcd0c1dceef
purplenimbus <you@example.com>
2017-02-24 07:43:12 +0000

    stage authentication services and controllers

---
+++ resources/assets/js/controllers/account.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jpApp.controller:AccountCtrl
+ * @description
+ * # AccountCtrl
+ * Controller of the jpApp
+ */
+angular.module('jpApp')
+	.controller('AccountCtrl', function ($scope,jobs,$route,$location,$filter,modal,elements,$rootScope,form,accountData)
+	{
+		
+	});
---
+++ resources/assets/js/services/accountData.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jpApp.controller:accountDataService
+ * @description
+ * # JobsCtrl
+ * Controller of the jpApp
+ */
+angular.module('jpApp')
+	.controller('accountDataService', function ($q,$http)
+	{
+		return {
+			accountData : function(){
+			
+			},
+			saveData 	:	function(){
+			
+			},
+			getJobs		:	function(){
+				
+			}
+		};
+	});
