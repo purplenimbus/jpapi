bd72f0057051ac3efbc0855227a267207afbfc78
purplenimbus <you@example.com>
2017-07-01 22:21:25 +0100

    controller update

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -27,21 +27,21 @@ class WordpressController extends Controller
     {
         $requests = $request->all();
 		
-		var_dump($requests);
-		
-		/*$model_type = $requests['jp_model'];
-		
-		//var_dump($model_type);
+		$model_type = $requests['jp_model'];
 		
 		echo "++++++++++++++++++++++++++++++++++++++++++++ \r\n"
 		
 		echo "WP ID : ".$wp_id." , ".$model_type." \r\n";
 		
+		/*
+		
+		//var_dump($model_type);
+		
 		//var_dump($requests);
 		
 		$model = DB::table($model_type)->get();*/
 		
-		return response()->json(['$requests' => $requests],200);
+		return response()->json(['model_type' => $model_type],200);
     }
 	
 }
