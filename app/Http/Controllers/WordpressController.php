68728811a19ff4a7e19722cb19dd6dc17d9b78e8
purplenimbus <you@example.com>
2017-07-01 20:30:31 +0100

    wordpress controller
    move all wordpress related requests to the wordpress controller

---
+++ app/Http/Controllers/WordpressController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
+class WordpressController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware('auth');
+    }
+	/**
+     * Store wordpress resources
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update($wp_id,Request $request)
+    {
+        $requests = $request->all();
+		
+		echo "WP ID : ".$wp_id." , ".$requests->jp_model." \r\n";
+		
+		var_dump($requests);
+    }
+}
