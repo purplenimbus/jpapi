33e1728bcd433d78fc6bc24bf0c91f74629120d0
purplenimbus <you@example.com>
2017-07-02 22:54:18 +0100

    controller update

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -90,7 +90,7 @@ class WordpressController extends Controller
 						echo "------------------------------------------------ \r\n";
 						var_dump($request[$key]);
 						
-						$data['jp_column'] = $this->get_jp_resource_id($request[$key]['wp_id'],$request[$key]['jp_model']);
+						$data[$request[$key]['jp_column']] = $this->get_jp_resource_id($request[$key]['wp_id'],$request[$key]['jp_model']);
 						
 						break;
 					
