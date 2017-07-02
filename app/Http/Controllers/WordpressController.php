be956711f76ad57b748575122118f64691cb8d00
purplenimbus <you@example.com>
2017-07-02 02:12:27 +0100

    controller update

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -70,7 +70,7 @@ class WordpressController extends Controller
 		
 		$wp_id 		= $request->wp_id;
 		
-		$sample_job = new Job;
+		$sample_job = new Job->getAttributes();
 							
 		$data = array();
 		
@@ -79,9 +79,9 @@ class WordpressController extends Controller
 			if($request->has($key)){
 				echo "key : ".$key."\r\n";
 				echo "Value Type : ".gettype($request[$key])."\r\n";
-				echo $key." exists in job model ?".isset($sample_job->$key)."\r\n";
+				echo $key." exists in job model ?".isset($sample_job[$key])."\r\n";
 				echo "----------------SAMPLE JOB------------------------- \r\n";
-				var_dump($sample_job->$key);
+				var_dump($sample_job[$key]);
 				echo "------------------------------------------------ \r\n";
 				
 				switch(gettype($request[$key])){
