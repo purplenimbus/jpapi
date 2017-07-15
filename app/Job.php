ed85d27f4a8dfd93a03175f6a71ed051409ec045
purplenimbus <you@example.com>
2017-07-15 19:02:55 +0100

    update job model to include compny_id

--- app/Job.php
+++ app/Job.php
@@ -14,7 +14,7 @@ class Job extends Model
     protected $fillable = [
         'title',
 		'description',
-		'job_company_id',
+		'company_id',
 		'job_salary_id',
 		'job_location_id',
 		'job_type_id',
