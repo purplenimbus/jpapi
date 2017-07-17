0c12d7ecf0d5cb41686a6201adb0ebe1248bb183
purplenimbus <you@example.com>
2017-07-17 07:47:03 +0100

    debug wordpress controller

--- database/migrations/2016_10_22_211046_create_locations_table.php
+++ database/migrations/2016_10_22_211046_create_locations_table.php
@@ -14,8 +14,8 @@ class CreateLocationsTable extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->increments('id');
-			$table->string('name')->nullable;
-			$table->string('locality')->nullable;
+			$table->string('name')->nullable();
+			$table->string('locality')->nullable();
 			$table->string('city',50);
 			$table->string('city_code',50)->nullable();
 			$table->string('state',50);
