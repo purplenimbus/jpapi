76be98c03b2a3ac63ff34291953692bc3c509aad
purplenimbus <you@example.com>
2017-07-17 08:47:05 +0100

    debug wordpress controller

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -76,7 +76,7 @@ class WordpressController extends Controller
 			//echo "++++++++++++++++++ MODEL INCOMING PARSED DATA +++++++++++++++++++++ \r\n";
 			
 			//get jpApi company id
-			if ($request->has('wp_company_id')) {
+			if (isset($request->has('wp_company_id'))) {
 				$data['company_id'] = $this->get_jp_resource_id($request->wp_company_id,'Company');
 			}
 						
