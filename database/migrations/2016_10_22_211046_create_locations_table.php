6f86ee43896bfbb30b162b6f04e961b8a1380e2a
purplenimbus <you@example.com>
2017-07-17 07:49:20 +0100

    update location migration

--- database/migrations/2016_10_22_211046_create_locations_table.php
+++ database/migrations/2016_10_22_211046_create_locations_table.php
@@ -14,14 +14,14 @@ class CreateLocationsTable extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->increments('id');
-			$table->string('name')->nullable();
+			//$table->string('name')->nullable();
 			$table->string('locality')->nullable();
 			$table->string('city',50);
-			$table->string('city_code',50)->nullable();
+			//$table->string('city_code',50)->nullable();
 			$table->string('state',50);
-			$table->string('state_code',50)->nullable();
+			//$table->string('state_code',50)->nullable();
 			$table->string('country',20);
-			$table->string('country_code',50)->nullable();
+			//$table->string('country_code',50)->nullable();
 			$table->string('zip_code',8)->nullable();
 			$table->string('url')->nullable();
 			$table->longText('description')->nullable();
