f4ab3f56745d45d41e9b0a4125756c5d7ef4d817
purplenimbus <you@example.com>
2017-07-24 07:28:47 +0100

    debug authenticate controller

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -77,104 +77,104 @@ class AuthenticateController extends Controller
 			//var_dump($accessTokenResponse->getBody());
 			
 			$accessToken = json_decode($accessTokenResponse->getBody(), true);
+		}catch(Exception $e){
+			return response()->json(['message' => $e->getMessage()],401);
+		}	
+		//echo "===================================================== \r\n";
+	
+		//echo "PROFILE RESPONSE \r\n";
+		try{
+			// Step 2. Retrieve profile information about the current user.
+			$profileResponse = $client->request('GET', 'https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,positions,picture-url,public-profile-url,industry)', [
+				'query' => [
+					'oauth2_access_token' => $accessToken['access_token'],
+					'format' => 'json'
+				]
+			]);
+			
+			//var_dump($profileResponse);
+		
+			//echo "---------------------------------------------------- \r\n";
+		
+			$profile = json_decode($profileResponse->getBody(), true);
+			
+			//echo "PROFILE \r\n";
+			
+			//var_dump($profile);
 			
 			//echo "===================================================== \r\n";
-		
-			//echo "PROFILE RESPONSE \r\n";
-			try{
-				// Step 2. Retrieve profile information about the current user.
-				$profileResponse = $client->request('GET', 'https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,positions,picture-url,public-profile-url,industry)', [
-					'query' => [
-						'oauth2_access_token' => $accessToken['access_token'],
-						'format' => 'json'
-					]
-				]);
 				
-				//var_dump($profileResponse);
+			//create new location
 			
-				//echo "---------------------------------------------------- \r\n";
+			//create new company
 			
-				$profile = json_decode($profileResponse->getBody(), true);
-				
-				//echo "PROFILE \r\n";
-				
-				//var_dump($profile);
-				
-				//echo "===================================================== \r\n";
-					
-				//create new location
-				
-				//create new company
-				
-				//save to wordpress
-				
-				// Step 3a. If user is already signed in then link accounts.
-				if ($request->header('Authorization'))
+			//save to wordpress
+			
+			// Step 3a. If user is already signed in then link accounts.
+			if ($request->header('Authorization'))
+			{
+				//echo "User Signed In \r\n";
+				$user = User::where('linkedin', '=', $profile['id']);
+				if ($user->first())
 				{
-					//echo "User Signed In \r\n";
-					$user = User::where('linkedin', '=', $profile['id']);
-					if ($user->first())
-					{
-						return response()->json(['message' => 'There is already a LinkedIn account that belongs to you'], 409);
-					}
-					$token = explode(' ', $request->header('Authorization'))[1];
-					$payload = (array) JWT::decode($token, Config::get('app.token_secret'), array('HS256'));
-					$user = User::find($payload['sub']);
-					$user->linkedin = $profile['id'];
-					$user->fname = $user->fname ?: $profile['firstName'];
-					$user->lname = $user->lname ?: $profile['lastName'];
-					$user->save();
-					return response()->json(['token' => $this->createToken($user)]);
+					return response()->json(['message' => 'There is already a LinkedIn account that belongs to you'], 409);
 				}
-				// Step 3b. Create a new user account or return an existing one.
-				else
-				{
-					//echo "User Not Signed In \r\n";
-					
-					
-					/*$user = User::updateOrCreate(['linkedin', $profile['id']],[
-												'fname' => $profile['firstName'],
-												'lname'	=>	$profile['lastName'],
-												'image_url'	=>	isset($profile['pictureUrl']) ? $profile['pictureUrl'] : '',
-											]);*/
-					
-					$user = User::where('linkedin', '=', $profile['id']);
-					if ($user->first())
-					{
-						return response()->json([ 'user' => $user , 'token' => JWTAuth::fromUser($user->first()) ]);
-					}else{
-						$user = new User;
-						$user->linkedin = $profile['id'];
-						$user->fname =  $profile['firstName'];
-						$user->lname =  $profile['lastName'];
-						$user->image_url =  isset($profile['pictureUrl']) ? $profile['pictureUrl'] : '';
-						$user->email =  isset($profile['emailAddress']) ? $profile['emailAddress'] : '';
-						
-						//save to mongo
-						$profile_data = $this->seeder->parse_user_profile($profile);
-						
-						/*echo "Profile Data \r\n";
-						
-						var_dump($profile_data);*/
-						
-						$resume = $this->seeder->save_profile($profile_data,$user->id);
-						
-						/*echo "Resume Data \r\n";
-						
-						var_dump($resume);*/
-						
-						$user->save();
-
-						return response()->json([ 'user' => $user , 'token' => JWTAuth::fromUser($user) , 'resume' => $resume ]);
-					}
-				}
-		
-			}catch(Exception $e){
-				return response()->json(['message' => $e->getMessage()],401);
+				$token = explode(' ', $request->header('Authorization'))[1];
+				$payload = (array) JWT::decode($token, Config::get('app.token_secret'), array('HS256'));
+				$user = User::find($payload['sub']);
+				$user->linkedin = $profile['id'];
+				$user->fname = $user->fname ?: $profile['firstName'];
+				$user->lname = $user->lname ?: $profile['lastName'];
+				$user->save();
+				return response()->json(['token' => $this->createToken($user)]);
 			}
+			// Step 3b. Create a new user account or return an existing one.
+			else
+			{
+				//echo "User Not Signed In \r\n";
+				
+				
+				/*$user = User::updateOrCreate(['linkedin', $profile['id']],[
+											'fname' => $profile['firstName'],
+											'lname'	=>	$profile['lastName'],
+											'image_url'	=>	isset($profile['pictureUrl']) ? $profile['pictureUrl'] : '',
+										]);*/
+				
+				$user = User::where('linkedin', '=', $profile['id']);
+				if ($user->first())
+				{
+					return response()->json([ 'user' => $user , 'token' => JWTAuth::fromUser($user->first()) ]);
+				}else{
+					$user = new User;
+					$user->linkedin = $profile['id'];
+					$user->fname =  $profile['firstName'];
+					$user->lname =  $profile['lastName'];
+					$user->image_url =  isset($profile['pictureUrl']) ? $profile['pictureUrl'] : '';
+					$user->email =  isset($profile['emailAddress']) ? $profile['emailAddress'] : '';
+					
+					//save to mongo
+					$profile_data = $this->seeder->parse_user_profile($profile);
+					
+					/*echo "Profile Data \r\n";
+					
+					var_dump($profile_data);*/
+					
+					$resume = $this->seeder->save_profile($profile_data,$user->id);
+					
+					/*echo "Resume Data \r\n";
+					
+					var_dump($resume);*/
+					
+					$user->save();
+
+					return response()->json([ 'user' => $user , 'token' => JWTAuth::fromUser($user) , 'resume' => $resume ]);
+				}
+			}
+	
 		}catch(Exception $e){
 			return response()->json(['message' => $e->getMessage()],401);
 		}
+
 		
 	}
 	
